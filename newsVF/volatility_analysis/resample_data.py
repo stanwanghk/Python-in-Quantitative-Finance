ef6fd9e7f76f87af52970a735e2dcf359c889c2c
stanwanghk <stan.wang.hk@gmail.com>
2017-04-25 10:23:45 +0800

    forecast

--- newsVF/volatility_analysis/resample_data.py
+++ newsVF/volatility_analysis/resample_data.py
@@ -4,33 +4,35 @@ import sqlite3
 import settings
 
 
-def pro_one_code(code='sz000001',return_col=['code','p_var','mean_return','num_news','sum_abs_sent']):
+def pro_one_code(code='sz000001', return_col=['code', 'p_var', 'mean_return', 'num_news', 'sum_abs_sent']):
     conn = sqlite3.connect(settings.get_sqlite_path())
     # process the stock
     sql = ''' SELECT  date,close,volume,p_change  FROM Stock 
               WHERE code like {}'''.format('\'' + code + '\'')
-    # print(sql)
-    raw = pd.read_sql(sql=sql,con=conn,index_col='date')
+    raw = pd.read_sql(sql=sql, con=conn, index_col='date')
     # raw = raw[columns]
     raw.index = pd.to_datetime(raw.index)
     draw = pd.DataFrame()
-    draw['p_var'] = raw['p_change'].resample('W',label='left').var()
-    draw['mean_return'] = raw['p_change'].resample('W',label='left').mean()
+    draw['p_var'] = raw['p_change'].resample('W', label='left').var()
+    draw['mean_return'] = raw['p_change'].resample('W', label='left').mean()
     draw = draw.assign(code=code)
 
     # process the news
     sql_news = '''  SELECT pubdate,source_id,sentiment FROM News LEFT OUTER JOIN Company_News 
                     ON News.id=Company_News.news_table_id
                     WHERE Company_News.company_id like {}'''.format('\'' + code + '\'')
-    raw_news = pd.read_sql(sql=sql_news, con=conn, index_col = 'pubdate')
+    raw_news = pd.read_sql(sql=sql_news, con=conn, index_col='pubdate')
     raw_news.index = pd.to_datetime(raw_news.index)
     raw_news['abs_sentiment'] = raw_news.sentiment.abs()
     draw_news = pd.DataFrame()
-    draw_news['sum_abs_sent'] = raw_news['abs_sentiment'].resample('W',label='left').sum()
-    draw_news['num_news'] = raw_news['sentiment'].resample('W',label='left').count()
+    draw_news['sum_abs_sent'] = raw_news[
+        'abs_sentiment'].resample('W', label='left').sum()
+    draw_news['num_news'] = raw_news[
+        'sentiment'].resample('W', label='left').count()
     # fill the NaN with 0 for news
     draw = draw.join(draw_news)
-    draw[['sum_abs_sent','num_news']] = draw[['sum_abs_sent','num_news']].fillna(0)
+    draw[['sum_abs_sent', 'num_news']] = draw[
+        ['sum_abs_sent', 'num_news']].fillna(0)
     # drop the NaN when the stock is not exchanged
     draw = draw.dropna()
     return draw[return_col]
@@ -49,4 +51,3 @@ def pro_all_codes():
         print('finish {}'.format(line[0]))
     raw.to_csv(home_path + 'data/week_data/total.csv')
     return raw
-    
--- newsVF/volatility_analysis/volatility_forecast.py
+++ newsVF/volatility_analysis/volatility_forecast.py
@@ -4,25 +4,31 @@ import pandas as pd
 import settings
 
 
-def model_select(input_col=['p_var', 'mean_return', 'sum_abs_sent']):
+def garch(input_col=['p_var', 'mean_return', 'sum_abs_sent'],
+          file_name='modified_grach'):
     # data = resample_data.process_all_codes()
-    data = pd.read_csv(settings.get_home_path() + 'data/week_data/total.csv', index_col='date')
+    data = pd.read_csv(settings.get_home_path() +
+                       'data/week_data/total.csv', index_col='date')
     indexs = data.index.drop_duplicates()
     # output_col = ['p_var_out']
-    file = open(settings.get_home_path() + "data/model_selection.csv", 'w')
-    file.write("svr_R^2,rfr_R^2,svr_trend,rfr_trend\n")
+    file = open(settings.get_home_path() +
+                'data/{}.csv'.format(file_name), 'w')
+    file.write(
+        "time_winodw_forecast_output,time_window_forecast_input,svr_R^2,rfr_R^2,\
+        svr_trend,rfr_trend\n")
     # validation
-    svr = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    svr = SVR(kernel='rbf', C=64, gamma=1 / 3)
     rfr = RFreg()
-    val_num = int(len(indexs) / 2)
-    # first train data
+
+    # the first train data
     train_input = data[data.index == indexs[0]].set_index('code')[input_col]
     train_output = data[data.index == indexs[1]].set_index('code')['p_var']
-    train = train_input.join(train_output, rsuffix='_out') 
+    train = train_input.join(train_output, rsuffix='_out')
     train = train.dropna()
-    for i in range(1, val_num):
+    for i in range(1, len(indexs)-2):
         test_input = data[data.index == indexs[i]].set_index('code')[input_col]
-        test_output = data[data.index == indexs[i + 1]].set_index('code')['p_var']
+        test_output = data[data.index == indexs[
+            i + 1]].set_index('code')['p_var']
         test = test_input.join(test_output, rsuffix='_out')
         test = test.dropna()
         svr.fit(train[input_col], train.p_var_out.values)
@@ -30,19 +36,29 @@ def model_select(input_col=['p_var', 'mean_return', 'sum_abs_sent']):
         test['p_var_pre'] = svr.predict(test[input_col])
         test['p_var_pre2'] = rfr.predict(test[input_col])
         # get the R squared
-        print(svr.score(train[input_col], train.p_var_out.values).round(4),
-                rfr.score(train[input_col], train.p_var_out.values).round(4))
+        r1 = svr.score(train[input_col], train.p_var_out.values).round(4)
+        r2 = rfr.score(train[input_col], train.p_var_out.values).round(4)
         # calculate the right rate for predicting trend
         test = test.assign(trend=0)
-        test.ix[(test.p_var_out>=test.p_var)&(test.p_var_pre>=test.p_var),'trend'] = 1
-        test.ix[(test.p_var_out<=test.p_var)&(test.p_var_pre<=test.p_var),'trend'] = 1
+        test.ix[(test.p_var_out >= test.p_var) & (
+            test.p_var_pre >= test.p_var), 'trend'] = 1
+        test.ix[(test.p_var_out <= test.p_var) & (
+            test.p_var_pre <= test.p_var), 'trend'] = 1
         test = test.assign(trend2=0)
-        test.ix[(test.p_var_out>=test.p_var)&(test.p_var_pre2>=test.p_var),'trend2'] = 1
-        test.ix[(test.p_var_out<=test.p_var)&(test.p_var_pre2<=test.p_var),'trend2'] = 1
-        print(test.trend.mean().round(4),test.trend2.mean().round(4))
-        file.write("{},{},{},{}\n".format(svr.score(train[input_col], train.p_var_out.values).round(4),
-            rfr.score(train[input_col],train.p_var_out.values).round(4),
-            test.trend.mean().round(4),
-            test.trend2.mean().round(4)))
+        test.ix[(test.p_var_out >= test.p_var) & (
+            test.p_var_pre2 >= test.p_var), 'trend2'] = 1
+        test.ix[(test.p_var_out <= test.p_var) & (
+            test.p_var_pre2 <= test.p_var), 'trend2'] = 1
+        t1 = test.trend.mean().round(4)
+        t2 = test.trend2.mean().round(4)
+        file.write("{},{},{},{},{},{}\n".format(
+            indexs[i + 1], indexs[i], r1, r2, t1, t2))
+        # save all predict vol
+        out = test[['p_var_pre', 'p_var_pre2', 'p_var_out']]
+        out = out.assign(date=indexs[i + 1])
+        out.to_csv(settings.get_home_path() +
+                   'data/total_predict_{}.csv'.format(file_name),
+                   mode='a', header=False, float_format='%.3f')
         train = test[input_col + ['p_var_out']]
+        print("finish: ", indexs[i])
     file.close()
