4bd9d7b91677ef7b26bb2ee651c1414952abc356
stanwanghk <stan.wang.hk@gmail.com>
2017-05-03 10:33:10 +0800

    H3

--- homework/mafs_6010G/assignment_3/size_analysis.py
+++ homework/mafs_6010G/assignment_3/size_analysis.py
@@ -16,8 +16,25 @@ def group_load(data,group=[0,2,5,10,15,20,30,40,55,90,100000]):
         print("finish: ", i)
 
 
-def plot_data(num):
-    data = pd.read_csv(home_path+"{}_group.csv".format(num),header=None)
-    data.columns = ['l','R_l']
-    plt.plot(data.l,data.R_l,label='{}'.format(num))
+def plot_data(group=[0,2,5,10,15,20,30,40,55,90,100000]):
+    plt.figure(figsize=(25,5))
+    for i in range(len(group)-1):
+        data = pd.read_csv(home_path+"{}_group.csv".format(group[i]),header=None)
+        data.columns = ['l','R_l']
+        plt.plot(data.l,data.R_l,':',label='{}'.format(group[i]))
+    # plt.legend(loc='upper left')
+    plt.xlabel("Lag")
+    plt.ylabel("Response function bechmarked to prevailing mid quote")
+    plt.title("Response fuction(in the unit of bid-ask spread of prevailing quote)")
+    plt.show()
     # return data
+
+def plot_q2():
+    data = pd.read_csv(home_path+"Q2_done.csv",header=None)
+    data.columns = ['l','R_l']
+    plt.figure(figsize=(15,5))
+    plt.plot(data.l,data.R_l,':')
+    plt.xlabel("Lag")
+    plt.ylabel("Response function bechmarked to prevailing mid quote")
+    plt.title("Response fuction(in the unit of bid-ask spread of prevailing quote)")
+    plt.show()
